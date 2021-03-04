2407420425f767c2cad589f8f8eef949ed69db6b
Krin <lightdark@bk.ru>
2021-03-04 17:17:46 +0300

    setTimeout вынесен в MovieList

--- src/components/small-movie-card/small-movies-card.jsx
+++ src/components/small-movie-card/small-movies-card.jsx
@@ -3,33 +3,14 @@ import PropTypes from "prop-types";
 import {movieType} from "../../utils/prop-types";
 import {Link} from "react-router-dom";
 import VideoPlayer from "../video-player/video-player";
-import {VIDEO_LOAD_TIMEOUT} from "../../consts/common";
 
 const SmallMovieCard = (props) => {
   const {movie, onHover, onCursorLeave, isActive} = props;
-  let timerID;
-
-  const articleRef = React.createRef();
-
-  React.useEffect(() => {
-
-    articleRef.current.onmouseenter = () => {
-      timerID = setTimeout(() => onHover(movie.id), VIDEO_LOAD_TIMEOUT);
-    };
-    articleRef.current.onmouseleave = () => {
-      clearTimeout(timerID);
-      onCursorLeave(movie.id);
-    };
-
-    return () => {
-      clearTimeout(timerID);
-    };
-  }, [isActive]);
 
   return (
-    <article ref={articleRef} className="small-movie-card catalog__movies-card"
-      // onMouseEnter={() => onHover(movie.id)}
-      // onMouseLeave={() => onCursorLeave(movie.id)}
+    <article className="small-movie-card catalog__movies-card"
+      onMouseEnter={() => onHover(movie.id)}
+      onMouseLeave={() => onCursorLeave(movie.id)}
     >
 
       <div className="small-movie-card__image">
