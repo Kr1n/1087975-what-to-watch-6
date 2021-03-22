c714fd030db838aa5437221acbea5846483e73e6
Krin <lightdark@bk.ru>
2021-03-22 09:58:27 +0300

    Флаг isFavorite теперь корректно работает (кроме промо фильма)

--- src/store/user/user.js
+++ src/store/user/user.js
@@ -1,8 +1,6 @@
-import {addToFavorite, requireAuthorization} from '../action';
+import {requireAuthorization} from '../action';
 import {createReducer} from '@reduxjs/toolkit';
 import {AuthorizationStatus} from '../../consts/common';
-import {adaptMoviesToClient} from "../../utils/utils";
-import {getMovieList} from "../movies-data/selectors";
 
 const initialState = {
   authorizationStatus: AuthorizationStatus.NO_AUTH,
@@ -15,19 +13,7 @@ const user = createReducer(initialState, (builder) => {
       authorizationStatus: action.payload,
     };
   });
-  builder.addCase(addToFavorite, (state, action) => {
-    const movie = adaptMoviesToClient([action.payload])[0];
-    const movieIndex = getMovieList(state).findIndex((item) => item.id === movie.id);
-    const updatedMovieList = getMovieList(state).slice();
-    updatedMovieList.splice(movieIndex, 1, movie);
 
-    return {
-      ...state,
-      movie,
-      movieList: updatedMovieList,
-      isFavoriteLoaded: false,
-    };
-  });
 });
 
 export {user};
