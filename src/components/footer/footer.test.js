e946a8ef289c61270598e93794934e4ebb05edd6
Krin <lightdark@bk.ru>
2021-03-24 17:14:44 +0300

    Исправлена ошибка в имени импортируемого файла.

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
-import Footer from './Footer';
+import Footer from './footer';
 
 it(`Footer page should render correctly`, () => {
   const history = createMemoryHistory();
