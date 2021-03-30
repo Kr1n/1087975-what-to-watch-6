a5a2e821aaa860650b7e2e3356ac089427ad73a6
Krin <lightdark@bk.ru>
2021-03-30 16:49:11 +0300

    Тест для компонента MyLsit

--- src/components/my-list/my-list.test.js
+++ src/components/my-list/my-list.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import * as redux from "react-redux";
@@ -8,34 +8,63 @@ import {MyList} from "./my-list";
 import {adaptMoviesToClient} from "../../utils/utils";
 
 const mockStore = configureStore({});
+const movie = adaptMoviesToClient([{
+  "id": 1,
+  "name": `The Grand Budapest Hotel`,
+  "poster_image": `img/the-grand-budapest-hotel-poster.jpg`,
+  "preview_image": `img/the-grand-budapest-hotel.jpg`,
+  "background_image": `img/the-grand-budapest-hotel-bg.jpg`,
+  "background_color": `#ffffff`,
+  "video_link": `https://some-link`,
+  "preview_video_link": `https://some-link`,
+  "description": `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
+  "rating": 8.9,
+  "scores_count": 240,
+  "director": `Wes Andreson`,
+  "starring": [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
+  "run_time": 99,
+  "genre": `Comedy`,
+  "released": 2014,
+  "is_favorite": false
+}])[0];
+const mockMovies = new Array(5).fill((movie))
+  .map((item, index) => ({...item, id: index, name: `Film name${index}`}));
 
-jest.mock(`../header/header`, () => <>This is mock Header</>);
-jest.mock(`../movie-list/movie-list`, () => <>This is mock MovieList</>);
+jest.mock(`./../header/header`, () =>{
+  const header = () => <>This is mock Header</>;
+  header.displayName = `Header`;
+  return {
+    __esModule: true,
+    default: () => {
+      return header();
+    }
+  };
+});
+
+jest.mock(`./../movie-list/movie-list`, () =>{
+  const movieList = () => <>This is mock MovieList</>;
+  movieList.displayName = `MovieList`;
+  return {
+    __esModule: true,
+    default: () => {
+      return movieList();
+    }
+  };
+});
+
+jest.mock(`./../footer/footer`, () =>{
+  const footer = () => <>This is mock Footer</>;
+  footer.displayName = `Footer`;
+  return {
+    __esModule: true,
+    default: () => {
+      return footer();
+    }
+  };
+});
 
 it(`MyList page should render correctly`, () => {
   const history = createMemoryHistory();
-
-  const movie = adaptMoviesToClient([{
-    "id": 1,
-    "name": `The Grand Budapest Hotel`,
-    "poster_image": `img/the-grand-budapest-hotel-poster.jpg`,
-    "preview_image": `img/the-grand-budapest-hotel.jpg`,
-    "background_image": `img/the-grand-budapest-hotel-bg.jpg`,
-    "background_color": `#ffffff`,
-    "video_link": `https://some-link`,
-    "preview_video_link": `https://some-link`,
-    "description": `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.`,
-    "rating": 8.9,
-    "scores_count": 240,
-    "director": `Wes Andreson`,
-    "starring": [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`, `Saoirse Ronan`],
-    "run_time": 99,
-    "genre": `Comedy`,
-    "released": 2014,
-    "is_favorite": false
-  }])[0];
-  const mockMovies = new Array(5).fill((movie))
-    .map((item, index) => ({...item, id: index, name: `Film name${index}`}));
 
   const store = mockStore({
     DATA: {
@@ -44,7 +73,7 @@ it(`MyList page should render correctly`, () => {
     }
   });
 
-  const {getByClassName} = render(
+  render(
       <redux.Provider store={store}>
         <Router history={history}>
           <MyList
@@ -56,6 +85,7 @@ it(`MyList page should render correctly`, () => {
       </redux.Provider>
   );
 
-  const myListElement = getByClassName(`catalog__movies-list`);
-  expect(myListElement).toBeInTheDocument();
+  expect(screen.getByText(/This is mock Header/i)).toBeInTheDocument();
+  expect(screen.getByText(/This is mock MovieList/i)).toBeInTheDocument();
+  expect(screen.getByText(/This is mock Footer/i)).toBeInTheDocument();
 });
